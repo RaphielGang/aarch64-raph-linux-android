dc7b300f6c21f75d329e2eefab181eb3ef2a9a4a
Raphiel Rollerscaperers <rapherion@raphielgang.org>
2019-07-03 23:16:38 +0700

    CI / spaceship : separate depinstall and spaceship
    Also while on it, set the script as executable
    
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>

---
+++ reqdeps.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2019 Raphielscape LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+# This file hold apt script to workaround Drone quirk
+
+apt-get install --yes \
+          git \
+          gcc \
+          g++ \
+          gperf \
+          bison \
+          flex \
+          texinfo \
+          help2man \
+          make \
+          libncurses5-dev \
+          autoconf \
+          automake \
+          libtool \
+          libtool-bin \
+          gawk \
+          wget \
+          bzip2 \
+          xz-utils \
+          unzip \
+          patch \
+          python3 \
+          libstdc++6
